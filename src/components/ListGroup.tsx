27da7b5dd8a2e75efea88b2075b949eb313701c0
jass <jasbrar080@gmail.com>
2024-01-17 14:43:26 -0800

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 function ListGroup(){
   // <></> these brackets represent fragment it is used to return more than one element
-  const items=[
+  let items=[
     "Mango",
     "Apple",
     "Orange",
     "Banana"
-  ]
+  ];
+  items=[];
   return (
     <>  
   <h1>Fruits</h1>
   <ul className="list-group">
+    {items.length===0 && <p>item not found</p>}
     {items.map(item=><li key={item}>{item}</li>)}
 </ul>
 </>
