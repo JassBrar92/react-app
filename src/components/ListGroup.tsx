9fa1ad69a7e531293716d9ae726d4a0749b6009c
jass <jasbrar080@gmail.com>
2024-01-17 15:04:22 -0800

    Handling Event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 function ListGroup(){
   // <></> these brackets represent fragment it is used to return more than one element
   let items=[
@@ -6,13 +7,16 @@ function ListGroup(){
     "Orange",
     "Banana"
   ];
-  items=[];
+  //items=[];
+ const handleClick=(event : MouseEvent)=>{
+    console.log(event);
+  }
   return (
     <>  
   <h1>Fruits</h1>
   <ul className="list-group">
     {items.length===0 && <p>item not found</p>}
-    {items.map(item=><li key={item}>{item}</li>)}
+    {items.map(item=><li className="list-group-item" key={item} onClick={handleClick}>{item}</li>)}
 </ul>
 </>
   );
