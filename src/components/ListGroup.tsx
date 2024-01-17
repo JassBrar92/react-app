18d2e714b8338eea188bdfa47684f8ddff52d191
jass <jasbrar080@gmail.com>
2024-01-17 14:32:24 -0800

    rendering list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,16 @@
 function ListGroup(){
   // <></> these brackets represent fragment it is used to return more than one element
+  const items=[
+    "Mango",
+    "Apple",
+    "Orange",
+    "Banana"
+  ]
   return (
     <>  
-  <h1>hello</h1>
+  <h1>Fruits</h1>
   <ul className="list-group">
-  <li className="list-group-item">An item</li>
-  <li className="list-group-item">A second item</li>
-  <li className="list-group-item">A third item</li>
-  <li className="list-group-item">A fourth item</li>
-  <li className="list-group-item">And a fifth one</li>
+    {items.map(item=><li key={item}>{item}</li>)}
 </ul>
 </>
   );
