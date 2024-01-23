517b52dda28f98ca7a3778dc48e68173f8b61702
jass <jasbrar080@gmail.com>
2024-01-22 18:40:16 -0800

    CSS in JS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
-import  './App.css';
+//import  './App.css';
 function App(){
   /*const [alertVisible,setAlertVisibility]=useState(false);
   return(
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,30 +1,40 @@
 import { useState } from "react";
-import style from './ListGroup.module.css'
+import  './ListGroup.css'
+import styled  from "styled-components";
 interface Props{
   items:string[],
   heading:string,
   onSelectItem:(item:string)=>void
 }
+interface ListItemProps{
+  active:boolean
+}
+const List=styled.ul`
+list-style: none;
+padding: 0px;
+`
+const ListItem=styled.li<ListItemProps>`
+padding:5px 0;
+background:${props=>props.active? 'blue':'none'}
+`
 function ListGroup({items,heading,onSelectItem}:Props){
   // <></> these brackets represent fragment it is used to return more than one element
-  const [selectedIndex,setSelectedIndex]=useState(-1);
+  const [selectedIndex,setSelectedIndex]=useState(0);
   
   return (
     <>  
   <h1>{heading}</h1>
-  <ul className={style.listGroup}>
+  <List>
     {items.length===0 && <p>item not found</p>}
-    {items.map((item,index)=><li className={selectedIndex===index
-    ?"list-group-item active"
-    :"list-group-item"
-    } 
+    {items.map((item,index)=><ListItem
+    active={index===selectedIndex}
     key={item} 
     onClick={()=>{
       setSelectedIndex(index); 
       onSelectItem(item);
     }
-    } >{item}</li>)}
-</ul>
+    } >{item}</ListItem>)}
+</List>
 </>
   );
 }
