7b1280f61a0d66034b04f8c40266bc6480568925
jass <jasbrar080@gmail.com>
2024-01-24 18:45:58 -0800

    add icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
+import { BsCalendar2DayFill } from "react-icons/bs";
 //import  './App.css';
 function App(){
   /*const [alertVisible,setAlertVisibility]=useState(false);
@@ -13,7 +14,8 @@ function App(){
   </Button>
   </div>
   )*/
-  const items=["Apple","Banana","Grapes","Orange"]
-  return <div><ListGroup heading="Fruits"items={items} onSelectItem={()=>{}}/></div>
+  //const items=["Apple","Banana","Grapes","Orange"]
+  //return <div><ListGroup heading="Fruits"items={items} onSelectItem={()=>{}}/></div>
+   return <BsCalendar2DayFill color="red" size={40}/>
 };
 export default App;
