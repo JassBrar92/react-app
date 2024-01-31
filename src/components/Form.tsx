98a1720ab028934a82ab02e692ec04d425c1abfe
jass <jasbrar080@gmail.com>
2024-01-30 19:04:02 -0800

    Handling form submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
 
 const Form = () => {
+  const handleSubmit=(event:FormEvent)=>{
+    event.preventDefault();
+    console.log("Submitted");
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3"><label htmlFor="name" className="form-label">Name</label>
       <input id='name' type="text" className="form-control" /></div>
       <div className="mb-3"><label htmlFor="age" className="form-label">Age</label>
       <input id='age' type="number" className="form-control" /></div>
+      <button className='btn-primary btn'>Submit</button>
     </form>
   )
 }
