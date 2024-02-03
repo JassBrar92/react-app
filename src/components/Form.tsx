c53f84cfbc75c03e8926c11491d4a16fe29c1087
jass <jasbrar080@gmail.com>
2024-02-03 12:24:33 -0800

    Shcema based validation with zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,28 @@
 import React, { FormEvent, useRef, useState } from 'react'
 import{FieldValues, useForm} from 'react-hook-form'
+import z from 'zod'
+import {zodResolver} from '@hookform/resolvers/zod'
 
+  const schema=z.object({
+    name:z.string().min(3,{message:'Name must be 3 characters long'}),
+    age:z.number({invalid_type_error:"Age field is required"}).min(18,{message:"Age should be at least 18"})
+  });
+  type FormData=z.infer<typeof schema>
 const Form = () => {
-       const {register,handleSubmit,formState:{errors}}=useForm();
+       const {register,handleSubmit,formState:{errors}}=useForm<FormData>({resolver:zodResolver(schema)});
        const onSubmit=(data: FieldValues)=>{
         console.log(data);
        }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3"><label htmlFor="name" className="form-label">Name</label>
-      <input {...register('name',{required:true,minLength:3})} id='name' type="text" className="form-control" />
-      {errors.name?.type==="required"&&<p className='text-danger'>Name is required.</p>}
-      {errors.name?.type==="minLength"&&<p className='text-danger'>Name must be atleast 3 characters long</p>}
+      <input {...register('name')} id='name' type="text" className="form-control" />
+      {errors.name&&<p className='text-danger'>{errors.name.message}</p>}
       </div>
       <div className="mb-3"><label htmlFor="age" className="form-label">Age</label>
-      <input {...register('age',{required:true})} 
+      <input {...register('age',{valueAsNumber:true})} 
        id='age' type="number" className="form-control" />
-       {errors.age?.type==="required"&&<p className='text-danger'>age is required</p>}
+       {errors.age&&<p className='text-danger'>{errors.age.message}</p>}
        </div>
       <button className='btn-primary btn'>Submit</button>
     </form>
